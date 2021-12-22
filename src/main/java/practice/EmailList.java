5657f5be4280e372edef8ce3b8c3a98f2e287c2d
Konstantin Shibkov <shibkov.k@gmail.com>
2021-12-22 17:08:58 +0500

    Initial commit

---
+++ src/main/java/practice/EmailList.java
@@ -0,0 +1,19 @@
+package practice;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class EmailList {
+
+    public void add(String email) {
+        // TODO: валидный формат email добавляется, email это строка, она быть может любой
+        // принять решение добавлять аргумент email или нет должен этот метод
+    }
+
+    public List<String> getSortedEmails() {
+        // TODO: возвращается сортированный список электронных адресов в алфавитном порядке
+        return new ArrayList<>();
+    }
+
+}
